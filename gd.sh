5762695f4c4a8f6578434e50646206bd7ec62c47
mizore <52705718+ukiyo99@users.noreply.github.com>
2020-05-27 16:59:16 +0800

    Update gd.sh

--- gd.sh
+++ gd.sh
@@ -25,13 +25,13 @@ fi
     ==<<极速转存即将开始，可ctrl+c中途中断>>=="
     echo 【开始拷贝】......
     #echo "gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M"
-    gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M
+    gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20
     echo 【查缺补漏】......
     #echo "gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M"
-    gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20 --min-size 10M
+    gclone copy goog:{$link} "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP --transfers=20
     echo 【去重检查】......
     #echo "gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP"
     gclone dedupe newest "goog:{myid}/$rootName" --drive-server-side-across-configs -vvP
     echo 【比对检查】......
     #echo "gclone check goog:{$link} "goog:{myid}/$rootName" --size-only --one-way --no-traverse"
-    gclone check goog:{$link} "goog:{myid}/$rootName" --size-only --one-way --no-traverse --min-size 10M
+    gclone check goog:{$link} "goog:{myid}/$rootName" --size-only --one-way --no-traverse
